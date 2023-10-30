a49234ff7a019618489577f3f19e754ed1592180
perry523 <wendel.renato@gmail.com>
2023-10-30 14:07:02 -0300

    ajustes

--- app.vue
+++ app.vue
@@ -6,13 +6,12 @@
 const broserLanguage = navigator.language;
 const { locale } = useI18n();
 const switchLocalePath = useSwitchLocalePath();
-// if(broserLanguage === )
 const router = useRouter();
-// if (broserLanguage === "pt-BR") {
-//   switchLocalePath("pt-BR");
-//   router.push("/");
-// } else {
-//   switchLocalePath("en-US");
-//   router.push("/en-US");
-// }
+if (broserLanguage === "pt-BR") {
+  switchLocalePath("pt-BR");
+  router.push("/");
+} else {
+  switchLocalePath("en-US");
+  router.push("/en-US");
+}
 </script>
